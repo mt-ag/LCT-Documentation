01758e453360bf2352e987441c59b3a3989e7ead
phartenfeller <pharti@hotmail.de>
2022-10-06 09:08:18 +0200

    initial commit

---
+++ docs/testing-quality/selectors.md
@@ -0,0 +1,7 @@
+---
+sidebar_position: 1
+---
+
+# Selectors
+
+Some helpers allow custom selectors as input. You can choose a working one in many different ways but there are best practices to choosing them in a way that is most resilient to changes to the page or APEX upgrades.
---
+++ docusaurus.config.js
@@ -0,0 +1,142 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+	title: "LCT Documentation",
+	tagline: "Making Testing APEX easy",
+	url: "https://your-docusaurus-test-site.com",
+	baseUrl: "/",
+	onBrokenLinks: "throw",
+	onBrokenMarkdownLinks: "warn",
+	favicon: "img/favicon.ico",
+
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	// organizationName: 'facebook', // Usually your GitHub org/user name.
+	// projectName: 'docusaurus', // Usually your repo name.
+
+	// Even if you don't use internalization, you can use this field to set useful
+	// metadata like html lang. For example, if your site is Chinese, you may want
+	// to replace "en" with "zh-Hans".
+	i18n: {
+		defaultLocale: "en",
+		locales: ["en"],
+	},
+
+	presets: [
+		[
+			"classic",
+			/** @type {import('@docusaurus/preset-classic').Options} */
+			({
+				docs: {
+					routeBasePath: "/",
+					sidebarPath: require.resolve("./sidebars.js"),
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl:
+						"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+				},
+				blog: false,
+				theme: {
+					customCss: require.resolve("./src/css/custom.css"),
+				},
+			}),
+		],
+	],
+
+	themeConfig:
+		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+		({
+			navbar: {
+				title: "Low Code Testing",
+				logo: {
+					alt: "LCT Logo",
+					src: "img/logo.svg",
+				},
+				items: [
+					// {
+					// 	type: "doc",
+					// 	docId: "intro",
+					// 	position: "left",
+					// 	label: "Tutorial",
+					// },
+					// { to: "/blog", label: "Blog", position: "left" },
+					{
+						href: "https://lct.software",
+						label: "LCT Website",
+						position: "right",
+					},
+				],
+			},
+			footer: {
+				style: "dark",
+				links: [
+					{
+						title: "Community",
+						items: [
+							{
+								label: "Twitter",
+								href: "https://twitter.com/LowCodeTesting",
+							},
+							{
+								label: "YouTube",
+								href: "https://www.youtube.com/channel/UCWj_laDAKjgRaw4SjEdVyIA",
+							},
+						],
+					},
+					{
+						title: "More",
+						items: [
+							{
+								label: "Homepage",
+								href: "https://lct.software",
+							},
+							{
+								label: "Blog",
+								href: "https://lct.software/blog",
+							},
+						],
+					},
+					{
+						title: "LCT by MT AG",
+						items: [
+							{
+								label: "MT Website",
+								href: "https://mt-ag.com",
+							},
+							{
+								label: "MT APEX Page",
+								href: "https://apex.mt-ag.com",
+							},
+						],
+					},
+				],
+				copyright: `Copyright © ${new Date().getFullYear()} MT AG. Built with Docusaurus.`,
+			},
+			prism: {
+				theme: lightCodeTheme,
+				darkTheme: darkCodeTheme,
+			},
+		}),
+	themes: [
+		// ... Your other themes.
+		[
+			require.resolve("@easyops-cn/docusaurus-search-local"),
+			{
+				// ... Your options.
+				// `hashed` is recommended as long-term-cache of index file is possible.
+				hashed: true,
+				// For Docs using Chinese, The `language` is recommended to set to:
+				// ```
+				// language: ["en", "zh"],
+				// ```
+			},
+		],
+	],
+};
+
+module.exports = config;
